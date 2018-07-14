306fc51e80beea75ebc39dffaa7ccb945801274f
Sungmin Kim <purpleblues568@gmail.com>
2018-07-15 00:14:10 +0900

    Initial Commit

---
+++ TerminalCalculator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TerminalCalculator
+//
+//  Created by Purple on 15/07/2018.
+//  Copyright © 2018 Purple. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
